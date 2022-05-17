261a49fa4e46e03e5fad42bdf31096a507a66779
Julian-Lop <you@example.com>
2022-05-17 16:35:07 -0500

    commit 06 profile pictures component design y stru

--- src/components/Fotos.jsx
+++ src/components/Fotos.jsx
@@ -3,10 +3,7 @@ import '../scss/components/Fotos.scss'
 export default function Fotos(){
     return(
         <div className="Fotos">
-            <h2>Fotos</h2>
-            <div className="PhotoContainer">
-
-            </div>
+            
         </div>
     )
 }
--- src/components/PublicationSection.jsx
+++ src/components/PublicationSection.jsx
@@ -4,11 +4,11 @@ import '../scss/components/PublicationSection.scss'
 export default function PublicationSection(){
     return (
         <div className="PublicationSection">
-            <hr></hr>
+            <hr className='Division'></hr>
             <div className='PublicationContainer'>
                 <Publication/>
             </div>
-            <hr></hr>
+            <hr className='Division'></hr>
         </div>
     )
 }
