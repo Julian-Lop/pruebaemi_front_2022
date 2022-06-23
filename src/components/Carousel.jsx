ec1cbfe20a766f74f5080f16e27b6e32e1a9e3ff
Julian-Lop <julian01andres@gmail.com>
2022-06-22 21:48:45 -0500

    diseño de carrousel mejorado

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,7 @@
 import Slider from 'infinite-react-carousel'
 import '../scss/components/Slider.scss'
 
-export default function Carousel({photos,setslider,slider}){
+export default function Carousel({photos,setslider,slider,initialSlide}){
 
     const setSlider = () => {
         setslider(!slider)
@@ -9,8 +9,8 @@ export default function Carousel({photos,setslider,slider}){
 
     return(
         <section className='Slider'>
-            <button onClick={() => setSlider()}><i class="fas fa-times"></i></button>
-            <Slider className="SliderContent">
+            <button onClick={() => setSlider()} className="exitButton"><i class="fas fa-times"></i></button>
+            <Slider className="SliderContent" initialSlide={initialSlide-1}>
                 {photos.length?photos.map(photo =>{
                     return(
                         <div className='SliderContentItem' key={photo.id} >
